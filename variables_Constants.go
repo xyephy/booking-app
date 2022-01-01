1ee8a45c395bc7139e6624c5fc523edb16d70c25
Толли <ziphyr.xyeag@gmail.com>
2022-01-01 20:11:04 +0300

    using Variables and Constants
    we use them for our application implementation

---
+++ variables_Constants.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+  // variable definition
+  var conferenceName = "Go Conference"
+  var  remainingTickets = 50
+  // constant definition
+  const conferenceTickets = 50
+  
+  fmt.Println("Welcome to", conferenceName, "booking application")
+  fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
+  fmt.Println("Get your tickets here to attend")
+  
+  
+  
+}
