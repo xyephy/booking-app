66ac9b233a3730e6af0086dc67f007e8c88ebddc
xyephy <ziphyr.xyeag@gmail.com>
2022-01-06 10:59:19 +0300

    fmt.Printf update

--- variables_Constants.go
+++ variables_Constants.go
@@ -3,16 +3,14 @@ package main
 import "fmt"
 
 func main() {
-  // variable definition
-  var conferenceName = "Go Conference"
-  var  remainingTickets = 50
-  // constant definition
-  const conferenceTickets = 50
-  
-  fmt.Println("Welcome to", conferenceName, "booking application")
-  fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available")
-  fmt.Println("Get your tickets here to attend")
-  
-  
-  
+	// variable definition
+	var conferenceName = "Go Conference"
+	var remainingTickets = 50
+	// constant definition
+	const conferenceTickets = 50
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
 }
