92666d26a5a8da8806a0eaaf6a3503beacf93ec0
xyephy <ziphyr.xyeag@gmail.com>
2022-01-14 10:21:34 +0300

    adding pointer
    The use of pointers allows us to store the user values in memory address

---
+++ pointers/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	// variable definition
+	conferenceName := "Go Conference" //short variable definition
+	var remainingTickets uint = 50
+	// constant definition
+	const conferenceTickets int = 50
+
+	fmt.Printf("conferenceTickets is %T, remainingTckets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets int
+
+	// ask user for data
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email name: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scan(&userTickets)
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+}
